87ac6118c6652f57518f22eb8693aa2512f4f516
Sergey Chelak <sergey.chelak@gmail.com>
2023-05-16 01:27:47 +0300

    clean up

--- src/main.rs
+++ src/main.rs
@@ -35,6 +35,7 @@ fn main() -> Result<(), String> {
 
     let texture_creator = canvas.texture_creator();
     let texture_manager = resources::TextureManager::new(&texture_creator);
+    
     let event_pump = sdl_context.event_pump()?;
 
     let color_manager = ColorManager;
@@ -52,69 +53,3 @@ fn main() -> Result<(), String> {
     );
     minesweeper.run()
 }
-
-/*
-fn game_loop(config: &Configuration, context: &mut SDLContext) -> Result<(), String> {
-    let canvas = &mut context.canvas;
-    let target_frame_duration = Duration::from_millis(1000u64 / config.target_fps());
-
-    let mut font_manager = resources::FontManager::new(&context.ttf_context);
-    let font = font_manager.font_header()?;
-    let mut texture_manager = resources::TextureManager::new(&context.texture_creator);
-    let image_1 = texture_manager.img_bomb()?;
-    'running: loop {
-        let frame_start_time = Instant::now();
-        for event in context.event_pump.poll_iter() {
-            match event {
-                Event::Quit { .. }
-                | Event::KeyDown {
-                    keycode: Some(Keycode::Escape),
-                    ..
-                } => break 'running,
-                _ => {}
-            }
-        }
-        {
-            canvas.set_draw_color(Color::RGB(0, 0, 0));
-            canvas.clear();
-            // TODO: draw code
-
-            // draw image
-            let h = 64u32;
-            let w = 64u32;
-            for i in 0..config.col_count() as u32 {
-                for j in 0..config.row_count() as u32 {
-                    let src = Rect::new(0, 0, w, h);
-                    let center = Point::new(w as i32 / 2, h as i32 / 2);
-                    let dest = Rect::new((i * w) as i32, (j * w) as i32, w, h);
-                    canvas.copy_ex(&image_1, src, dest, 0.0, center, false, false)?;
-                }
-            }
-
-            // draw text
-            let text = "In development".to_string();
-            let surface = font
-                .render(&text)
-                .blended(Color::RGB(240, 0, 30))
-                .map_err(|e| e.to_string())?;
-            let texture = context
-                .texture_creator
-                .create_texture_from_surface(&surface)
-                .map_err(|e| e.to_string())?;
-
-            let texture_target = Rect::new(130, 20, 650, 150);
-            canvas.copy(&texture, None, Some(texture_target))?;
-
-            canvas.present();
-        }
-        let elapsed_time = frame_start_time.elapsed();
-        let sleep_time = target_frame_duration.saturating_sub(elapsed_time);
-        if sleep_time.is_zero() {
-            println!("Frame elapsed time {elapsed_time:?}");
-            continue;
-        }
-        ::std::thread::sleep(sleep_time);
-    }
-    Ok(())
-}
- */
